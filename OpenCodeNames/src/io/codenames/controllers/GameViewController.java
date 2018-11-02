1226d64287d6f192b452559e686adea01d90d6f5
BuddhiAbeyratne <buddhi741@gmail.com>
2018-11-03 02:18:08 +0530

    Make GameViewController Initializable

--- OpenCodeNames/src/io/codenames/controllers/GameViewController.java
+++ OpenCodeNames/src/io/codenames/controllers/GameViewController.java
@@ -1,4 +1,24 @@
-package io.codenames;
+package io.codenames.controllers;
 
-public class Controller {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+public class GameViewController implements Initializable {
+	
+	@FXML
+	private JFXButton chatSend;
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
 }
