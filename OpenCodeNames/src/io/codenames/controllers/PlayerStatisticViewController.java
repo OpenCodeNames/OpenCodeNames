420fb70308aafc99fb96109ba44462a8d81a93f1
hasitha de silva <hasithadman@gmail.com>
2018-11-03 13:26:29 +0530

    Created button click event for player statistic

---
+++ OpenCodeNames/src/io/codenames/controllers/PlayerStatisticViewController.java
@@ -0,0 +1,33 @@
+package io.codenames.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+public class PlayerStatisticViewController implements Initializable {
+	
+	@FXML 
+	protected void playerStatsAction(ActionEvent event) {
+		try {
+				ViewController viewcontroller = ViewController.getInstance();
+				viewcontroller.addScreen("PlayerStats", FXMLLoader.load(getClass().getResource( "/fxml/PlayerStatsView.fxml" )));
+				viewcontroller.activate("PlayerStats");
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+}
