c04f5709cba85ca7637141e2384c67db316bbc55
BuddhiAbeyratne <buddhi741@gmail.com>
2018-11-11 15:16:51 +0530

    Implemented user registration on the client side

--- OpenCodeNames/src/io/codenames/controllers/UserRegistrationViewController.java
+++ OpenCodeNames/src/io/codenames/controllers/UserRegistrationViewController.java
@@ -2,26 +2,50 @@ package io.codenames.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.rmi.Naming;
 import java.util.ResourceBundle;
 
+import io.codenames.serverinterfaces.PlayersHandlerInterface;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
 
 public class UserRegistrationViewController implements Initializable  {
 
+	private PlayersHandlerInterface playerhandler;
+
+	@FXML
+	TextField userName;
+
+	@FXML
+	TextField userPass;
+
+	@FXML
+	TextField userConfirmPass;
 	/**
 	 * Action listener for User Registration button
 	 * @param event
 	 */
-	
 	@FXML 
 	protected void registerAction(ActionEvent event) {
 		try {
-				ViewController viewcontroller = ViewController.getInstance();
-				viewcontroller.addScreen("LoginView", FXMLLoader.load(getClass().getResource( "/fxml/LoginView.fxml" )));
-				viewcontroller.activate("LoginView");
+			if(!userPass.getText().equals(userConfirmPass.getText())){
+				JOptionPane.showMessageDialog(new JFrame(), "Password doesn't match confirm pass", "Error",
+						JOptionPane.ERROR_MESSAGE);
+			}else {
+				if (playerhandler.register(userName.getText(), userPass.getText())) {
+					ViewController viewcontroller = ViewController.getInstance();
+					viewcontroller.addScreen("LoginView", FXMLLoader.load(getClass().getResource("/fxml/LoginView.fxml")));
+					viewcontroller.activate("LoginView");
+				}else{
+					JOptionPane.showMessageDialog(new JFrame(), "User by that username already Exists", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
 		}
 		catch (IOException e) {
 			e.printStackTrace();
@@ -52,6 +76,10 @@ public class UserRegistrationViewController implements Initializable  {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		try{
+			playerhandler = (PlayersHandlerInterface) Naming.lookup("rmi://localhost/PlayersHandler");
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
 	}
 }
