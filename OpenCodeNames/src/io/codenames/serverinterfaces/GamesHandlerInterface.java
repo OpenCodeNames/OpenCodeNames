8f68936deafa4963cc5c0b6c2c17bd0e466596ac
hasitha de silva <hasithadman@gmail.com>
2018-11-11 16:03:42 +0530

    Clone from Server Interface

--- OpenCodeNames/src/io/codenames/serverinterfaces/GamesHandlerInterface.java
+++ OpenCodeNames/src/io/codenames/serverinterfaces/GamesHandlerInterface.java
@@ -4,5 +4,15 @@ import java.rmi.*;
 
 public interface GamesHandlerInterface extends Remote{
 
+	public boolean createGame(String gameName, String createrName, int playerCount);
 	
+	public boolean joinGame(String gameName, String playerName);
+	
+	public String getCodeNameOfCard(String gameName, int i);
+	
+	public boolean revealCard(String gameName, String playerName, int i);
+	
+	public boolean placeChatMessage(String gameName, String platerName, String message);
+	
+	public boolean placeHintMessage(String gameName, String playerName, String message);
 }
