4bd5a1a2082ec4e53bb6ec0c3f7c8e914bdb9570
BuddhiAbeyratne <buddhi741@gmail.com>
2018-11-11 15:16:12 +0530

    Implemented Authentication and setup Cookies for saving username

--- OpenCodeNames/src/io/codenames/controllers/LoginViewController.java
+++ OpenCodeNames/src/io/codenames/controllers/LoginViewController.java
@@ -2,16 +2,30 @@ package io.codenames.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.rmi.Naming;
 import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
 
+import io.codenames.serverinterfaces.PlayersHandlerInterface;
 import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
 
 public class LoginViewController implements Initializable {
-	
+
+    private PlayersHandlerInterface playerhandler;
+    private Preferences pref;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private TextField userPass;
 	/**
 	 * Action listener for login button
 	 * @param event
@@ -19,14 +33,22 @@ public class LoginViewController implements Initializable {
 	@FXML 
 	protected void handleLoginAction(ActionEvent event) {
 		try {
-			ViewController viewcontroller = ViewController.getInstance();
-			viewcontroller.addScreen("Menu", FXMLLoader.load(getClass().getResource( "/fxml/MenuView.fxml" )));
-			viewcontroller.activate("Menu");
+		    if(playerhandler.authenticate(userName.getText(),userPass.getText())) {
+                this.pref.put("userName", userName.getText());
+                ViewController viewcontroller = ViewController.getInstance();
+                viewcontroller.addScreen("Menu", FXMLLoader.load(getClass().getResource("/fxml/MenuView.fxml")));
+                viewcontroller.activate("Menu");
+            }else{
+                JOptionPane.showMessageDialog(new JFrame(), "User Name and Password incorrect", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
 		} catch (IOException e) {
 			e.printStackTrace();
 		}catch (RuntimeException e) {
 			e.printStackTrace();
-		}
+		}catch (Exception e){
+            e.printStackTrace();
+        }
 	}
 	
 	/**
@@ -48,6 +70,11 @@ public class LoginViewController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+        try{
+            this.pref = Preferences.userNodeForPackage(io.codenames.Main.class);
+            playerhandler = (PlayersHandlerInterface) Naming.lookup("rmi://localhost/PlayersHandler");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 	}
 }
