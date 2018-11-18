28e4b552d7c074bfaf152eebb9c9bde2f68e966e
BuddhiAbeyratne <buddhi741@gmail.com>
2018-11-18 16:29:36 +0530

    connect gameview with client invoker singleton

--- OpenCodeNames/src/io/codenames/controllers/GameViewController.java
+++ OpenCodeNames/src/io/codenames/controllers/GameViewController.java
@@ -1,14 +1,12 @@
 package io.codenames.controllers;
 
-import java.io.IOException;
 import java.net.URL;
+import java.rmi.RemoteException;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
 public class GameViewController implements Initializable {
@@ -19,6 +17,12 @@ public class GameViewController implements Initializable {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		try {
+			ClientCommandInvoker cci = ClientCommandInvoker.getInstance();
+			cci.setGameScreen(this);
+
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 }
