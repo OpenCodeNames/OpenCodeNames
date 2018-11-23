a070f006908ea6071d11313437248ff2b2afa4b3
hasitha de silva <hasithadman@gmail.com>
2018-11-23 21:44:35 +0530

    Implemented RMI for player statistics

--- OpenCodeNames/src/io/codenames/controllers/PlayerStatsController.java
+++ OpenCodeNames/src/io/codenames/controllers/PlayerStatsController.java
@@ -2,12 +2,21 @@ package io.codenames.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.util.ResourceBundle;
+import java.util.prefs.Preferences;
 
+import io.codenames.data.Game;
+import io.codenames.serverdata.Player;
+import io.codenames.serverinterfaces.GamesHandlerInterface;
+import io.codenames.serverinterfaces.PlayersHandlerInterface;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * Action listener for back button
@@ -15,7 +24,27 @@ import javafx.fxml.Initializable;
  */
 
 public class PlayerStatsController implements Initializable{
-	@FXML 
+	
+	
+	private PlayersHandlerInterface playerhandler;
+	private Preferences pref;
+	
+	@FXML
+	private Label playerName;
+	
+	@FXML
+	private Label correctCards;
+	
+	@FXML
+	private Label wrongCards;
+	
+	@FXML
+	private Label deathCards;
+	
+	@FXML
+	private Label winRate;
+	
+	@FXML
 	protected void backAction(ActionEvent event) {
 		try {
 				ViewController viewcontroller = ViewController.getInstance();
@@ -32,6 +61,27 @@ public class PlayerStatsController implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
+		try{
+			this.pref = Preferences.userNodeForPackage(io.codenames.Main.class);
+			playerhandler = (PlayersHandlerInterface) Naming.lookup("rmi://"+pref.get("rmiUri", "localhost")+"/PlayersHandler");
+		}catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+
+		playerName.setText(this.pref.get("userName",""));
+		String playerName = this.pref.get("userName","");
+		try {
+			Player player = playerhandler.getPlayer(playerName);
+			int cardReviled = player.getCardsReviled();
+			correctCards.setText(player.getCorrectReviles()+"/"+cardReviled);
+			wrongCards.setText(player.getIncorrectReviles()+"/"+cardReviled);
+			deathCards.setText(player.getDeathCards()+"/"+cardReviled);
+			winRate.setText(player.getGamesWon()+"/"+player.getNumGames());
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
+
