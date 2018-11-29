eb7193d4d347850a1f23eb139f99a61650e5f546
BuddhiAbeyratne <buddhi741@gmail.com>
2018-11-29 12:13:39 +0530

    added icons for the jar file

--- OpenCodeNames/src/io/codenames/Main.java
+++ OpenCodeNames/src/io/codenames/Main.java
@@ -9,17 +9,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	Preferences pref;
     @Override
     public void start(Stage primaryStage) throws Exception{
+
     	this.pref = Preferences.userNodeForPackage(io.codenames.Main.class);
     	String rmiUri = JOptionPane.showInputDialog("Enter RMI Server IP",pref.get("rmiUri", ""));
         this.pref.put("rmiUri", rmiUri);
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginView.fxml"));
         primaryStage.setTitle("CodeNames");
+        primaryStage.getIcons().add(new Image("/Icons/Icon.jpg"));
         Scene main = new Scene(root, 1000, 600);
         main.getStylesheets().add("/css/style.css");
         
