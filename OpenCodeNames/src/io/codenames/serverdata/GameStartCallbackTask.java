d6be18c6c20a33c14096e125f8279837bba18ab5
BuddhiAbeyratne <buddhi741@gmail.com>
2018-12-02 02:49:51 +0530

    Cloned from server

---
+++ OpenCodeNames/src/io/codenames/serverdata/GameStartCallbackTask.java
@@ -0,0 +1,27 @@
+package io.codenames.serverdata;
+
+import java.rmi.RemoteException;
+import java.util.Map;
+import java.util.TimerTask;
+
+public class GameStartCallbackTask extends TimerTask {
+
+    Map<String, PlayerProxy> players;
+
+
+    public GameStartCallbackTask(Map<String, PlayerProxy> players) {
+        this.players = players;
+    }
+
+    @Override
+    public void run() {
+        for (Map.Entry<String,PlayerProxy> entry : players.entrySet() ) {
+            try {
+                PlayerProxy player = entry.getValue();
+                player.getClientCallBackInterface().startGame();
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
